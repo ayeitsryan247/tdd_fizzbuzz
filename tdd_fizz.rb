aa9e5c6234e1b6d9567fd053ae99a51bc3ec9e7e
ayeitsryan247 <39201054+ayeitsryan247@users.noreply.github.com>
2018-06-20 13:39:17 -0400

    Setup teast again

--- tdd_fizz.rb
+++
@@ -1,6 +0,0 @@
-def fizzbuzz()
- arr = [*1..100]
- arr[2] = "mind"
- arr[25] = "minds"
- arr
-end
--- tdd_test_fizz.rb
+++ tdd_test_fizz.rb
@@ -7,21 +7,5 @@ class TestTddFizz < Minitest::Test
     assert_equal(1, 1)
  end
 
- def test_result_is_array
-   assert_equal(Array, fizzbuzz().class)
- end
-
- def test_array_is_100
-    assert_equal(100, fizzbuzz().length)
-  end
-
-  def test_array_first_elem
-    assert_equal(1, fizzbuzz()[0])
-  end
-
-  def test_array_3rd_elem_returns_mind
-    assert_equal("mind", fizzbuzz()[2])
-    assert_equal("minds", fizzbuzz()[25])
-  end
 
 end
