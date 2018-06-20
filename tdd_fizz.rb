1d91225d13d149a8ed7ab0b2db318223787b5378
ayeitsryan247 <39201054+ayeitsryan247@users.noreply.github.com>
2018-06-20 15:20:52 -0400

    array fixed

--- tdd_fizz.rb
+++ tdd_fizz.rb
@@ -1,5 +1,14 @@
-def fizbuzz()
- fizzarr = [*1..100]
- fizzarr[2] = "mined"
- fizzarr
+def fizzbuzz()
+	fizzarr = [*1..100]
+	fizzarr[2] = "mined"
+	fizzarr[4] = "minds"
+	fizzarr[14] = "mined minds"
+	new_arr = []
+	fizzarr.each do |item|
+		if item % 3 == 0
+			item = "mined"
+		end
+		new_arr << item
+	end
+	new_arr
 end
--- tdd_test_fizz.rb
+++ tdd_test_fizz.rb
@@ -1,30 +1,40 @@
 require "minitest/autorun"
 require_relative "tdd_fizz.rb"
 
-class TestTddFizz < Minitest::Test
+class TestTddFizzBuzz < Minitest::Test
 
- def test_assert_that_1_equals_1
-    assert_equal(1, 1)
- end
+    def test_assert_that_1_equals_1
+        assert_equal(1, 1)
+    end
 
- def test_end_res_is_array
-   assert_equal(Array, fizbuzz().class)
-end
+    def test_assert_that_end_result_is_array
+    	assert_equal(Array, fizzbuzz().class)
+    end
 
-def test_assert_array_length_is_100
-    assert_equal(100, fizbuzz().length)
-  end
+    def test_assert_array_length_is_100
+    	assert_equal(100, fizzbuzz().length)
+    end
 
-def test_assert_1test_assert_1st_position
-  assert_equal(1, fizbuzz().first)
-end
+    def test_assert_first_position_is_1
+    	assert_equal(1, fizzbuzz().first)
+    end
 
-def test_assert_third_position_is_mined
-  assert_equal("mined", fizbuzz()[2])
-end
+    def test_assert_third_position_is_mined
+    	assert_equal("mined", fizzbuzz()[2])
+    end
 
-def test_assert_5_position_minds
-  assert_equal("mines", fizzbuzz()[4])
-end
+	def test_assert_fifth_position_is_minds
+		assert_equal("minds", fizzbuzz()[4])
+	end
+
+	def test_assert_fifteenth_position_is_mined_minds
+		assert_equal("mined minds", fizzbuzz()[14])
+	end
+
+	def test_assert_multiples_of_3_returns_mined
+		assert_equal("mined", fizzbuzz()[5])
+		assert_equal("mined", fizzbuzz()[32])
+	end
+
 
 end
