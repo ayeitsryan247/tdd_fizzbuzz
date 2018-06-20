0b6ef5339766aa8aa6ce45bf074e1645663a4235
ayeitsryan247 <39201054+ayeitsryan247@users.noreply.github.com>
2018-06-20 15:40:44 -0400

    array new error

--- tdd_fizz.rb
+++ tdd_fizz.rb
@@ -1,11 +1,6 @@
 def fizzbuzz()
 	fizzarr = [*1..100]
-	fizzarr[2] = "mined"
-	fizzarr[4] = "minds"
-	fizzarr[14] = "mined minds"
-  fizzarr[4] = "minds"
-  fizzarr[9] = "minds"
-  fizzarr[24] = "minds"
+
 	new_arr = []
 	fizzarr.each do |item|
 		if item % 3 == 0
--- tdd_test_fizz.rb
+++ tdd_test_fizz.rb
@@ -3,43 +3,45 @@ require_relative "tdd_fizz.rb"
 
 class TestTddFizzBuzz < Minitest::Test
 
-    def test_assert_that_1_equals_1
+    def test_assert_that_1_equals_1()
         assert_equal(1, 1)
     end
 
-    def test_assert_that_end_result_is_array
+    def test_assert_that_end_result_is_array()
     	assert_equal(Array, fizzbuzz().class)
     end
 
-    def test_assert_array_length_is_100
+    def test_assert_array_length_is_100()
     	assert_equal(100, fizzbuzz().length)
     end
 
-    def test_assert_first_position_is_1
+    def test_assert_first_position_is_1()
     	assert_equal(1, fizzbuzz().first)
     end
 
-    def test_assert_third_position_is_mined
+    def test_assert_third_position_is_mined()
     	assert_equal("mined", fizzbuzz()[2])
     end
 
-	def test_assert_fifth_position_is_minds
+	def test_assert_fifth_position_is_minds()
 		assert_equal("minds", fizzbuzz()[4])
 	end
 
-	def test_assert_fifteenth_position_is_mined_minds
+	def test_assert_fifteenth_position_is_mined_minds()
 		assert_equal("mined minds", fizzbuzz()[14])
 	end
 
-	def test_assert_multiples_of_3_returns_mined
+	def test_assert_multiples_of_3_returns_mined()
 		assert_equal("mined", fizzbuzz()[5])
 		assert_equal("mined", fizzbuzz()[32])
 	end
-  def test_array_every_5th_element_returns_minds
+  def test_array_every_5th_element_returns_minds()
     assert_equal("minds", fizzbuzz()[4])
     assert_equal("minds", fizzbuzz()[9])
     assert_equal("minds", fizzbuzz()[24])
 end
+
+
 
 
 end
