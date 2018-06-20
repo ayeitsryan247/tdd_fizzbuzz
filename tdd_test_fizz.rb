20390fb5761e5e023c812cef8267a980a2a7c998
ayeitsryan247 <39201054+ayeitsryan247@users.noreply.github.com>
2018-06-20 11:58:25 -0400

    Unit testing test fail

--- tdd_test_fizz.rb
+++ tdd_test_fizz.rb
@@ -3,13 +3,15 @@ require_relative "tdd_fizz.rb"
 
 class TestTddFizz < Minitest::Test
 
-    def test_assert_that_1_equals_1
-        assert_equal(1, 1)
-    end
+ def test_assert_that_1_equals_1
+    assert_equal(1, 1)
+ end
 
-    def test_result_is_array
-      assert_equal(Array, fizzbuzz().class)
+ def test_result_is_array
+   assert_equal(Array, fizzbuzz().class)
+ end
 
-    end
-
+ def test_array_is_100
+    assert_equal(100, fizzbuzz().length)
+  end
 end
