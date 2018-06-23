5a7bc3cfae185727e7d4fa4de10de2d545438eef
ayeitsryan247 <39201054+ayeitsryan247@users.noreply.github.com>
2018-06-23 16:01:47 -0400

    replacing 4 throwing error due to handle not being given

---
+++ test/tdd_fizz.rb
@@ -0,0 +1,17 @@
+
+def fizzbuzz()
+	fizzarr = [*1..100]
+
+	new_arr = []
+	fizzarr.each do |item|
+		if item %3 == 0 && item %5 == 0
+      item = "mined minds"
+    elsif item %3 == 0
+			item = "mined"
+    elsif item %5 == 0
+      item = "minds"
+		end
+		new_arr << item
+	end
+	new_arr
+end
